fb3036ecaf36ce6a36ab590a65a624296314a1f3
Erwin van Hunen <erwin.van.hunen@outlook.com>
2025-06-14 01:59:02 +0200

    cleanup

--- src/breaktimer-preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('electronAPI', {
-  closeBreakTimer: () => ipcRenderer.send('close-break-timer'),
-  getSettings: () => ipcRenderer.invoke('get-settings'),
-});
