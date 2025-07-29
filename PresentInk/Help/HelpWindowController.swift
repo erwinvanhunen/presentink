6a0f2b9b1d55efe7edc058e292f38d102c1f7012
Erwin van Hunen <erwin.van.hunen@outlook.com>
2025-07-29 18:59:14 +0200

    layout updates

--- PresentInk/Help/HelpWindowController.swift
+++ PresentInk/Help/HelpWindowController.swift
@@ -10,6 +10,64 @@ class HelpWindowController: NSWindowController {
     }
 
     convenience init() {
+
+        func shortcutRow(keys: [String], desc: String, color: NSColor? = nil)
+            -> [NSView]
+        {
+
+            let keyStack = NSStackView()
+            keyStack.orientation = .horizontal
+            keyStack.spacing = 6
+            for (i, k) in keys.enumerated() {
+                keyStack.addArrangedSubview(KbdView(text: k, fontSize: 10))
+                if i < keys.count - 1 {
+                    let plus = NSTextField(labelWithString: "+")
+                    plus.font = NSFont.systemFont(ofSize: 16)
+                    plus.textColor = NSColor.tertiaryLabelColor
+                    keyStack.addArrangedSubview(plus)
+                }
+            }
+            keyStack.translatesAutoresizingMaskIntoConstraints = false
+            keyStack.setContentHuggingPriority(.required, for: .horizontal)
+            keyStack.setContentCompressionResistancePriority(
+                .required,
+                for: .horizontal
+            )
+
+            let labelStack = NSStackView()
+            labelStack.orientation = .horizontal
+            labelStack.spacing = 8
+
+            if let color = color {
+                let colorBox = NSView(
+                    frame: NSRect(x: 0, y: 0, width: 18, height: 18)
+                )
+                colorBox.wantsLayer = true
+                colorBox.layer?.backgroundColor = color.cgColor
+                colorBox.layer?.cornerRadius = 4
+                colorBox.translatesAutoresizingMaskIntoConstraints = false
+                colorBox.widthAnchor.constraint(equalToConstant: 18).isActive =
+                    true
+                colorBox.heightAnchor.constraint(equalToConstant: 18).isActive =
+                    true
+                labelStack.addArrangedSubview(colorBox)
+            }
+
+            let label = NSTextField(labelWithString: desc)
+            label.font = NSFont.systemFont(ofSize: 12)
+            label.textColor = NSColor.labelColor
+            label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            label.setContentCompressionResistancePriority(
+                .defaultLow,
+                for: .horizontal
+            )
+            label.lineBreakMode = .byWordWrapping
+            label.maximumNumberOfLines = 0
+            label.preferredMaxLayoutWidth = 0
+            labelStack.addArrangedSubview(label)
+            return [keyStack, labelStack]
+        }
+
         let size = NSSize(width: 800, height: 540)
         let window = NSWindow(
             contentRect: NSRect(origin: .zero, size: size),
@@ -154,143 +212,187 @@ class HelpWindowController: NSWindowController {
             shortcutGrid.addRow(with: v)
         }
         shortcutGrid.column(at: 1).xPlacement = .fill
-//        shortcutGrid.column(at: 1).width = .greaterThanOrEqualToConstant(0)
-      
-        func shortcutRow(keys: [String], desc: String, color: NSColor? = nil)
-            -> [NSView]
-        {
+        shortcutGrid.column(at: 0).xPlacement = .trailing
+        shortcutGrid.column(at: 0).width = 180
 
-            let keyStack = NSStackView()
-            keyStack.orientation = .horizontal
-            keyStack.spacing = 6
-            for (i, k) in keys.enumerated() {
-                keyStack.addArrangedSubview(KbdView(text: k, fontSize: 10))
-                if i < keys.count - 1 {
-                    let plus = NSTextField(labelWithString: "+")
-                    plus.font = NSFont.systemFont(ofSize: 16)
-                    plus.textColor = NSColor.tertiaryLabelColor
-                    keyStack.addArrangedSubview(plus)
-                }
-            }
-            keyStack.translatesAutoresizingMaskIntoConstraints = false
-            keyStack.setContentHuggingPriority(.required, for: .horizontal)
-            keyStack.setContentCompressionResistancePriority(
-                .required,
-                for: .horizontal
-            )
-
-            let labelStack = NSStackView()
-            labelStack.orientation = .horizontal
-            labelStack.spacing = 8
-
-            if let color = color {
-                let colorBox = NSView(
-                    frame: NSRect(x: 0, y: 0, width: 18, height: 18)
+        let drawShortCuts: [[NSView]] = [
+            shortcutRow(
+                keys: ["Esc"],
+                desc: NSLocalizedString(
+                    "Exit draw, screenshot or break timer mode",
+                    tableName: "Help",
+                    comment: ""
                 )
-                colorBox.wantsLayer = true
-                colorBox.layer?.backgroundColor = color.cgColor
-                colorBox.layer?.cornerRadius = 4
-                colorBox.translatesAutoresizingMaskIntoConstraints = false
-                colorBox.widthAnchor.constraint(equalToConstant: 18).isActive =
-                    true
-                colorBox.heightAnchor.constraint(equalToConstant: 18).isActive =
-                    true
-                labelStack.addArrangedSubview(colorBox)
-            }
+            ),
+            shortcutRow(
+                keys: ["Shift"],
+                desc: NSLocalizedString(
+                    "Draw perfectly straight lines",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Cmd", "Shift"],
+                desc: NSLocalizedString(
+                    "Draw arrows",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Cmd"],
+                desc: NSLocalizedString(
+                    "Draw boxes (rectangles)",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Option"],
+                desc: NSLocalizedString(
+                    "Draw an ellipse",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Option", "Shift"],
+                desc: NSLocalizedString(
+                    "Draw a centered ellipse",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Control"],
+                desc: NSLocalizedString(
+                    "Draw with a marker/highlighter",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["E"],
+                desc: NSLocalizedString(
+                    "Clear all drawings but stay in draw mode",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["T"],
+                desc: NSLocalizedString(
+                    "Add text",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Cmd", "Z"],
+                desc: NSLocalizedString(
+                    "Undo last drawing",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Cmd", "Shift", "Z"],
+                desc: NSLocalizedString(
+                    "Redo last drawing",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["r"],
+                desc: NSLocalizedString(
+                    "Change color to red",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .red
+            ),
+            shortcutRow(
+                keys: ["g"],
+                desc: NSLocalizedString(
+                    "Change color to green",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .green
+            ),
+            shortcutRow(
+                keys: ["b"],
+                desc: NSLocalizedString(
+                    "Change color to blue",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .blue
+            ),
+            shortcutRow(
+                keys: ["y"],
+                desc: NSLocalizedString(
+                    "Change color to yellow",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .yellow
+            ),
+            shortcutRow(
+                keys: ["o"],
+                desc: NSLocalizedString(
+                    "Change color to orange",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .orange
+            ),
+            shortcutRow(
+                keys: ["p"],
+                desc: NSLocalizedString(
+                    "Change color to pink",
+                    tableName: "Help",
+                    comment: ""
+                ),
+                color: .magenta
+            ),
+            shortcutRow(
+                keys: ["w"],
+                desc: NSLocalizedString(
+                    "Activate whiteboard mode (draw on a white background)",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["k"],
+                desc: NSLocalizedString(
+                    "Activate blackboard mode (draw on a black background)",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+            shortcutRow(
+                keys: ["Space"],
+                desc: NSLocalizedString(
+                    "Center the cursor on the current screen",
+                    tableName: "Help",
+                    comment: ""
+                )
+            ),
+        ]
 
-            let label = NSTextField(labelWithString: desc)
-            label.font = NSFont.systemFont(ofSize: 12)
-            label.textColor = NSColor.labelColor
-            label.setContentHuggingPriority(.defaultLow, for: .horizontal)
-            label.setContentCompressionResistancePriority(
-                .defaultLow,
-                for: .horizontal
-            )
-            label.lineBreakMode = .byWordWrapping
-            label.maximumNumberOfLines = 0
-            label.preferredMaxLayoutWidth = 0
-            labelStack.addArrangedSubview(label)
-            return [keyStack, labelStack]
-        }
-
-    
-                let drawShortCuts: [[NSView]] = [
-                    shortcutRow(
-                        keys: ["Esc"],
-                        desc: NSLocalizedString("Exit draw, screenshot or break timer mode", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(keys: ["Shift"], desc: NSLocalizedString("Draw perfectly straight lines", tableName: "Help", comment:"")),
-                    shortcutRow(keys: ["Cmd", "Shift"], desc: NSLocalizedString("Draw arrows", tableName: "Help", comment:"")),
-                    shortcutRow(keys: ["Cmd"], desc: NSLocalizedString("Draw boxes (rectangles)", tableName: "Help", comment:"")),
-                    shortcutRow(keys: ["Option"], desc: NSLocalizedString("Draw an ellipse", tableName: "Help", comment:"")),
-                    shortcutRow(
-                        keys: ["Option", "Shift"],
-                        desc: NSLocalizedString("Draw a centered ellipse", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(
-                        keys: ["Control"],
-                        desc: NSLocalizedString("Draw with a marker/highlighter", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(
-                        keys: ["E"],
-                        desc: NSLocalizedString("Clear all drawings but stay in draw mode", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(keys: ["T"], desc: NSLocalizedString("Add text", tableName: "Help", comment:"")),
-                    shortcutRow(keys: ["Cmd", "Z"], desc: NSLocalizedString("Undo last drawing", tableName: "Help", comment:"")),
-                    shortcutRow(
-                        keys: ["Cmd", "Shift", "Z"],
-                        desc: NSLocalizedString("Redo last drawing", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(
-                        keys: ["r"],
-                        desc: NSLocalizedString("Change color to red", tableName: "Help", comment:""),
-                        color: .red
-                    ),
-                    shortcutRow(
-                        keys: ["g"],
-                        desc: NSLocalizedString("Change color to green", tableName: "Help", comment:""),
-                        color: .green
-                    ),
-                    shortcutRow(
-                        keys: ["b"],
-                        desc: NSLocalizedString("Change color to blue", tableName: "Help", comment:""),
-                        color: .blue
-                    ),
-                    shortcutRow(
-                        keys: ["y"],
-                        desc: NSLocalizedString("Change color to yellow", tableName: "Help", comment:""),
-                        color: .yellow
-                    ),
-                    shortcutRow(
-                        keys: ["o"],
-                        desc: NSLocalizedString("Change color to orange", tableName: "Help", comment:""),
-                        color: .orange
-                    ),
-                    shortcutRow(
-                        keys: ["p"],
-                        desc: NSLocalizedString("Change color to pink", tableName: "Help", comment:""),
-                        color: .magenta
-                    ),
-                    shortcutRow(
-                        keys: ["w"],
-                        desc: NSLocalizedString("Activate whiteboard mode (draw on a white background)", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(
-                        keys: ["k"],
-                        desc: NSLocalizedString("Activate blackboard mode (draw on a black background)", tableName: "Help", comment:"")
-                    ),
-                    shortcutRow(
-                        keys: ["Space"],
-                        desc: NSLocalizedString("Center the cursor on the current screen", tableName: "Help", comment:"")
-                    ),
-                ]
-        
         let drawShortCutsGrid = NSGridView()
         drawShortCutsGrid.rowSpacing = 12
+
         for (_, v) in drawShortCuts.enumerated() {
             drawShortCutsGrid.addRow(with: v)
         }
-        
+        drawShortCutsGrid.column(at: 1).xPlacement = .fill
+        drawShortCutsGrid.column(at: 0).xPlacement = .trailing
+        drawShortCutsGrid.column(at: 0).width = 180
         contentStack.addArrangedSubview(title)
         contentStack.addArrangedSubview(desc)
         contentStack.addArrangedSubview(keyboardTitle)
@@ -304,17 +406,29 @@ class HelpWindowController: NSWindowController {
                 comment: ""
             )
         )
+        let spacer = NSView()
+        spacer.translatesAutoresizingMaskIntoConstraints = false
+        spacer.heightAnchor.constraint(equalToConstant: 8).isActive = true
+        contentStack.addArrangedSubview(spacer)
         drawTitle.font = NSFont.boldSystemFont(ofSize: 16)
         drawTitle.textColor = NSColor.labelColor
 
         contentStack.addArrangedSubview(drawTitle)
         contentStack.addArrangedSubview(drawShortCutsGrid)
         let scrollView = NSScrollView(frame: window.contentView!.bounds)
-        scrollView.hasVerticalScroller = true
-        scrollView.hasHorizontalScroller = false
-        scrollView.autoresizingMask = [.width, .height]
+        scrollView.contentView.addSubview(contentStack)
+        NSLayoutConstraint.activate([
+            contentStack.leadingAnchor.constraint(
+                equalTo: scrollView.contentView.leadingAnchor
+            ),
+            contentStack.trailingAnchor.constraint(
+                equalTo: scrollView.contentView.trailingAnchor
+            ),
+            contentStack.topAnchor.constraint(
+                equalTo: scrollView.contentView.topAnchor
+            ),
+        ])
         scrollView.documentView = contentStack
-
         window.contentView = scrollView
 
         self.init(window: window)
@@ -329,17 +443,6 @@ class HelpWindowController: NSWindowController {
         )
         contentStack.insertArrangedSubview(buyMeACoffeeButton, at: 2)
 
-        DispatchQueue.main.async {
-            if let documentView = scrollView.documentView {
-                let topPoint = NSPoint(
-                    x: 0,
-                    y: documentView.bounds.height
-                        - scrollView.contentView.bounds.height
-                )
-                scrollView.contentView.scroll(to: topPoint)
-                scrollView.reflectScrolledClipView(scrollView.contentView)
-            }
-        }
     }
 
     private func shortcutRow(
